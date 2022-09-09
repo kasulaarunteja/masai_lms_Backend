b70683d2fdb5e860e4fcd9dfe88419ac4c79911d
kasulaarunteja <kasulaarunteja@gmail.com>
2022-09-09 14:52:17 +0530

    lectures

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ const connect = require('./config/db');
 const app = express();
 // const router = express.Router();
 const cors = require('cors');
-require('dotenv').config()
-const {register, login} = require("./controllers/auth.controller")
+require('dotenv').config();
+const {Lectures, router}= require("./controllers/Lectures.controller");
+const {register, login} = require("./controllers/auth.controller");
 
 
 app.use(cors());
@@ -16,6 +17,7 @@ app.use(express.json());
 require("dotenv").config();
 app.post('/register', register)
 app.post('/login', login)
+app.use("/", router);
 
 
 const PORT = process.env.PORT || 8080;
