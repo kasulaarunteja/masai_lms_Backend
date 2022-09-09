4c2b2b136ae14cfe83ca2c98343582aef3ff02aa
kasulaarunteja <kasulaarunteja@gmail.com>
2022-09-09 13:38:53 +0530

    Auth

---
+++ src/app.js
@@ -0,0 +1,31 @@
+
+
+const express = require("express");
+const connect = require('./config/db');
+
+
+const app = express();
+// const router = express.Router();
+const cors = require('cors');
+require('dotenv').config()
+const {register, login} = require("./controllers/auth.controller")
+
+
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+app.post('/register', register)
+app.post('/login', login)
+
+
+const PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT , async () => {
+    try {
+        await connect()
+    }catch(error){
+        console.log('err')
+    }
+    console.log(`listing ${PORT}`)
+})
